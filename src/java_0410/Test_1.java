e6404fa127d74e8074f1fb6d47ac2552c8e9f8f5
waleilulu <waleilulu@gmail.com>
2025-04-10 10:20:45 +0800

    0410

--- src/java_0410/Test_1.java
+++ src/java_0410/Test_1.java
@@ -4,21 +4,30 @@ import java.util.Scanner;
 
 public class Test_1 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.print("請輸入成績: ");
-        int num = sc.nextInt();
-        if (num >= 90 && num <= 100) {
-            System.out.println("A");
-        } else if (num >= 80) {
-            System.out.println("B");
-        } else if (num >= 70) {
-            System.out.println("C");
-        } else if (num >= 60) {
-            System.out.println("D");
-        } else if (num >= 0) {
-            System.out.println("D");
-        } else{
-            System.out.println("輸入錯誤，請重新輸入!");
+        while(true){
+            System.out.print("請輸入成績: ");
+            int num = sc.nextInt();
+
+            if (num >= 90 && num <= 100) {
+                System.out.println("A");
+                break;
+            } else if (num >= 80 && num < 90) {
+                System.out.println("B");
+                break;
+            } else if (num >= 70 && num < 80) {
+                System.out.println("C");
+                break;
+            } else if (num >= 60 && num < 70) {
+                System.out.println("D");
+                break;
+            } else if (num >= 0 && num <60) {
+                System.out.println("F");
+                break;
+            } else{
+                System.out.println("輸入錯誤，請重新輸入!");
+            }
         }
         sc.close();
     }
