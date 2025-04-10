d5f3573068d1fe9dc4f2d009c33b99934eec6370
meiwata <waleilulu@gmail.com>
2025-04-10 11:45:29 +0800

    整數、浮點數

---
+++ src/java_0410/Test_2.java
@@ -0,0 +1,12 @@
+package java_0410;
+
+public class Test_2 {
+    public static void main(String[] args) {
+        int div = 5/2;
+        System.out.println(div);
+
+//        double div2 = 5/2; // 2.0
+        double div2 = 5.0/2.0; // 2.5
+        System.out.println(div2);
+    }
+}
