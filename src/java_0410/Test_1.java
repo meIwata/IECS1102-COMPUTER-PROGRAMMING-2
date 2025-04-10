4acc2d4e34398d317e6592858f09f7a2a4df884f
meiwata <waleilulu@gmail.com>
2025-04-10 10:57:24 +0800

    do...while

--- src/java_0410/Test_1.java
+++ src/java_0410/Test_1.java
@@ -5,35 +5,34 @@ import java.util.Scanner;
 public class Test_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while (true) {
+        int num;
+        do {
             System.out.print("請輸入成績: ");
-
             // 如果不是整數就提示並跳過
             while (!sc.hasNextInt()) {
                 System.out.print("輸入錯誤，請重新輸入: ");
                 sc.next();
             }
 
-            int num = sc.nextInt();
+            num = sc.nextInt();
             if (num >= 90 && num <= 100) {
                 System.out.println("A");
-                break;
             } else if (num >= 80 && num < 90) {
                 System.out.println("B");
-                break;
+
             } else if (num >= 70 && num < 80) {
                 System.out.println("C");
-                break;
+
             } else if (num >= 60 && num < 70) {
                 System.out.println("D");
-                break;
-            } else if (num >= 0 && num <60) {
+
+            } else if (num >= 0 && num < 60) {
                 System.out.println("F");
-                break;
-            } else{
+
+            } else {
                 System.out.print("輸入錯誤，");
             }
-        }
+        } while (num < 0 || num > 100);
         sc.close();
     }
 }
