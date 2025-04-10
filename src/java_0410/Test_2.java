22597f03610a490ff5ab477e233da313de272285
meiwata <waleilulu@gmail.com>
2025-04-10 11:49:17 +0800

    強制轉型

--- src/java_0410/Test_2.java
+++ src/java_0410/Test_2.java
@@ -8,5 +8,10 @@ public class Test_2 {
 //        double div2 = 5/2; // 2.0
         double div2 = 5.0/2.0; // 2.5
         System.out.println(div2);
+
+        double num = 5.3;
+        int div3=(int)num; // 強制轉型
+        System.out.println(div3);
+
     }
 }
