b790a56c90580802bf9248e5a4a8942e84938fd0
meiwata <waleilulu@gmail.com>
2025-04-15 10:59:31 +0800

    0415

--- src/java_0415/TestPokemon.java
+++ src/java_0415/TestPokemon.java
@@ -8,13 +8,19 @@ public class TestPokemon {
         duck.hp = 200;
         duck.candy = 180;
         duck.printInfo();
+        duck.powerUp();
+        duck.printInfo();
+        duck.evolve();
+        duck.printInfo();
 
-
+        System.out.println("--------------------");
         Pokemon fog = new Pokemon();
         fog.name = "妙蛙種子";
         fog.cp = 50;
         fog.hp = 80;
         fog.candy = 100;
+        fog.printInfo();
+        fog.evolve();
         fog.printInfo();
     }
 }
