f99a357772d87f98259f3d4aa48a2b0a18345c69
meiwata <waleilulu@gmail.com>
2025-04-17 11:36:59 +0800

    GUI介面

--- src/java_0417/oneGUI.java
+++ src/java_0417/oneGUI.java
@@ -23,18 +23,70 @@ public class oneGUI {
         b0.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                int result = bucket1.put_knifes(8);
-                if (result == 1) {
-//                    System.out.println("成功");
-                    b0.setText("成功");
-                } else if (result == 0) {
-                    //System.out.println("失敗");
-                    b0.setText("失敗");
-                } else if (result == -1) {
-//                    System.out.println("爆, 失敗");
-                    b0.setText("爆, 失敗");
-                }
-                b0.setEnabled(false); // 按完就不能再按
+                btnBehavior(b0);
+            }
+        });
+
+        b1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                btnBehavior(b1);
+            }
+        });
+
+        b2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                btnBehavior(b2);
+            }
+        });
+
+        b3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                btnBehavior(b3);
+            }
+        });
+
+        b4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                btnBehavior(b4);
+            }
+        });
+
+        b5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                btnBehavior(b5);
+            }
+        });
+
+        b6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                btnBehavior(b6);
+            }
+        });
+
+        b7.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                btnBehavior(b7);
+            }
+        });
+
+        b8.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                btnBehavior(b8);
+            }
+        });
+
+        b9.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                btnBehavior(b9);
             }
         });
     }
@@ -48,5 +100,21 @@ public class oneGUI {
         frame.setSize(200,600);
         frame.setLocation(480,300);
         bucket1 = new bucket();
+    }
+
+    public void btnBehavior(JButton btn){
+        int number = Integer.parseInt(btn.getText()); // 把按鈕的文字取出，再轉乘整數
+        int result = bucket1.put_knifes(number);
+        if (result == 1) {
+//                    System.out.println("成功");
+            btn.setText("成功");
+        } else if (result == 0) {
+            //System.out.println("失敗");
+            btn.setText("失敗");
+        } else if (result == -1) {
+//                    System.out.println("爆, 失敗");
+            btn.setText("爆, 失敗");
+        }
+        btn.setEnabled(false); // 按完就不能再按
     }
 }
