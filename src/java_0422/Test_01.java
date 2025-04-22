fb17e53fde52b6fbb144e730d7592ea463ecd60f
meiwata <waleilulu@gmail.com>
2025-04-22 09:55:53 +0800

    0422

---
+++ src/java_0422/Test_01.java
@@ -0,0 +1,14 @@
+package java_0422;
+
+public class Test_01 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 15;
+        System.out.println("a: " + a + ";b: " + b);
+
+        b = a;
+
+        b = 25;
+        System.out.println("a: " + a + ";b: " + b);
+    }
+}
