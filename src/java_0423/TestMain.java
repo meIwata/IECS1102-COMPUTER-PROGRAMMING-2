6c95fb17c1891916964cbe6e743d138a769d5214
waleilulu <waleilulu@gmail.com>
2025-04-23 23:42:28 +0800

    0423

--- src/java_0423/TestMain.java
+++ src/java_0423/TestMain.java
@@ -3,11 +3,18 @@ package java_0423;
 public class TestMain {
     public static void main(String[] args) {
         Dog dog = new Dog("旺仔");
-        Parrot parrot = new Parrot("鳥仔");
-
         dog.sleep();
         dog.action1("跟其他狗打架!");
         System.out.println(dog.action2("跟其他狗打架!"));
+        dog.walk();
+        dog.makeSound();
+        dog.eat();
+        dog.wagTail();
+        dog.fetchBone();
+        dog.perform();
+        dog.name = "小白";
+        System.out.println("旺仔已經改名成" + dog.name + "了!!!");
 
+        Parrot parrot = new Parrot("鳥仔");
     }
 }
