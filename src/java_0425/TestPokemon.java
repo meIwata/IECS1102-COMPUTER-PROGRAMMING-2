9af94d4b6e7d3c6a1b11dabf5cf221b16556d1ee
meIwata <waleilulu@gmail.com>
2025-04-25 10:02:33 +0800

    0425

---
+++ src/java_0425/TestPokemon.java
@@ -0,0 +1,9 @@
+package java_0425;
+
+public class TestPokemon {
+    public static void main(String[] args) {
+        Pokemon pikachu = new Pokemon();
+        pikachu.powerUp();
+        pikachu.printCpValue();
+    }
+}
