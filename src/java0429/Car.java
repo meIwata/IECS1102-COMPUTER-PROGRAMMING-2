c120b6e89918db6ac497cd02e6f9a1b8618c372e
meiwata <waleilulu@gmail.com>
2025-04-29 09:31:49 +0800

    無參數建構方法

---
+++ src/java0429/Car.java
@@ -0,0 +1,20 @@
+package java0429;
+
+public class Car {
+    String carNumber; // 預設值: null
+    String color; // 預設值: null
+    int doors; // 預設值: 0
+    int seats; // 預設值: 0
+
+    Car(){ // 系統預設的無參數的建構方法
+
+    }
+
+    void drive(){
+
+    }
+
+    void stop(){
+
+    }
+}
---
+++ src/java0429/TestCar.java
@@ -0,0 +1,10 @@
+package java0429;
+
+public class TestCar {
+    public static void main(String[] args) {
+        Car toyota = new Car(); // 指的是Car(){}
+        System.out.println(toyota.carNumber);
+        System.out.println(toyota.doors);
+    }
+
+}
