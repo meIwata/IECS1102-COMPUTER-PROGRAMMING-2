7bf08a63a4bd0b6526f1660955847c159828fc27
meiwata <waleilulu@gmail.com>
2025-04-29 09:43:43 +0800

    有參數建構方法

--- src/java0429/Car.java
+++ src/java0429/Car.java
@@ -1,6 +1,7 @@
 package java0429;
 
 public class Car {
+    // 成員變數
     String carNumber; // 預設值: null
     String color; // 預設值: null
     int doors; // 預設值: 0
@@ -14,11 +15,23 @@ public class Car {
         seats = 5;
     }
 
+    // 有參數建構方法
+    Car(String carNumber, String color, int doors, int seats){
+        this.carNumber = carNumber; // this.carNumber 指的是上面的成員變數
+        this.color = color;
+        this.doors = doors;
+        this.seats = seats;
+    }
+
     void drive(){
 
     }
 
     void stop(){
 
+    }
+
+    void printCar(){
+        System.out.println(carNumber + ";" + color + ";" + doors + ";" + seats);
     }
 }
--- src/java0429/TestCar.java
+++ src/java0429/TestCar.java
@@ -3,8 +3,12 @@ package java0429;
 public class TestCar {
     public static void main(String[] args) {
         Car toyota = new Car(); // 指的是Car(){}
-        System.out.println(toyota.carNumber);
-        System.out.println(toyota.doors);
+//        System.out.println(toyota.carNumber);
+//        System.out.println(toyota.doors);
+
+        // 使用者自行輸入參數
+        Car bmw = new Car("BBB-1234", "black", 5, 7);
+        bmw.printCar();
     }
 
 }
