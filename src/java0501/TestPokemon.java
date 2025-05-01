4ac6a5c61cbbb7d09e5a070b1329e72b10f935d6
meiwata <waleilulu@gmail.com>
2025-05-01 10:00:51 +0800

    變數的存取修飾值、建構方法、Getter & Setter、toString

--- src/java0501/TestPokemon.java
+++ src/java0501/TestPokemon.java
@@ -2,7 +2,7 @@ package java0501;
 
 public class TestPokemon {
     public static void main(String[] args) {
-        // ⚠️重點⚠️ 一般方法需透過創出實體物件去呼叫
+        // ⚠️重點⚠️ 一般方法是綁在物件上，所以需透過創出實體物件(one)去呼叫
         Pokemon one = new Pokemon();
         one.setName("皮卡丘");
         one.setCp(10);
@@ -13,7 +13,7 @@ public class TestPokemon {
 //        int a = one.getCp();
 //        System.out.println(a);
 
-        // ⚠️重點⚠️ 靜態方法可以用類別做呼叫
+        // ⚠️重點⚠️ 靜態方法可以用類別(Pokemon)做呼叫
         Pokemon.printString("Hello FCU");
 
 
