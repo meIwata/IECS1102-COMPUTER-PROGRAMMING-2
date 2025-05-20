592509ab30d2df5ef7f77729d61646bd052fc3a2
meiwata <waleilulu@gmail.com>
2025-05-20 10:46:29 +0800

    補充==的用法

--- src/jave0515/Gym.java
+++ src/jave0515/Gym.java
@@ -3,35 +3,35 @@ package jave0515;
 import java.util.Random;
 
 public class Gym {
-    // 傳入兩隻神起寶貝，讓其對戰
-    // 贏家規則
-    // 1. level高的贏
-    // 2. 若level相同，則比較AttackPower - DefencePower的值
-    // 3. 若2. 相同，則隨機選出一個贏家
-    // 最後印出贏家的資訊
+	// 傳入兩隻神起寶貝，讓其對戰
+	// 贏家規則
+	// 1. level高的贏
+	// 2. 若level相同，則比較AttackPower - DefencePower的值
+	// 3. 若2. 相同，則隨機選出一個贏家
+	// 最後印出贏家的資訊
 
-    // H.W. 用隨機的方式傳入兩隻神奇寶貝對戰
+	// H.W. 用隨機的方式傳入兩隻神奇寶貝對戰
 
-    public static void fight(Pokemon x, Pokemon y) {
-        if (x.getLevel() > y.getLevel()) {
-            System.out.println(x.getName() + "勝");
-        } else if (x.getLevel() < y.getLevel()) {
-            System.out.println(y.getName() + "勝");
-        } else { // level 相同
-            int xPower = x.attackPower - x.defencePower;
-            int yPower = y.attackPower - y.defencePower;
-            if (xPower > yPower) {
-                System.out.println(x.getName() + "勝");
-            } else if (xPower < yPower) {
-                System.out.println(y.getName() + "勝");
-            } else {
-                // 隨機挑出 x 或 y
-                String[] chosenArray = {x.getName(), y.getName()};
-                Random random = new Random();
-                int index = random.nextInt(2); // 產生一個 0 或 1 的隨機整數
-                System.out.println(chosenArray[index] + "勝");
+	public static void fight(Pokemon x, Pokemon y) {
+		if (x.getLevel() > y.getLevel()) {
+			System.out.println(x.getName() + "勝");
+		} else if (x.getLevel() < y.getLevel()) {
+			System.out.println(y.getName() + "勝");
+		} else { // level 相同
+			int xPower = x.attackPower - x.defencePower;
+			int yPower = y.attackPower - y.defencePower;
+			if (xPower > yPower) {
+				System.out.println(x.getName() + "勝");
+			} else if (xPower < yPower) {
+				System.out.println(y.getName() + "勝");
+			} else {
+				// 隨機挑出 x 或 y
+				String[] chosenArray = {x.getName(), y.getName()};
+				Random random = new Random();
+				int index = random.nextInt(2); // 產生一個 0 或 1 的隨機整數
+				System.out.println(chosenArray[index] + "勝");
 
-                // 老師的方法
+				// 老師的方法
                 /*
                 double random2 = Math.random(); // 0~1，不包含1
                 if (random2 > 0.5) {
@@ -40,12 +40,24 @@ public class Gym {
                     System.out.println(y.getName() + "勝");
                 }
                 */
-            }
-        }
-    }
+			}
+		}
+	}
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+
+		/*補充==的用法*/
+		Pokemon p1 = new Pokemon("鴨頭", 10, 15, 20, 30);
+		Pokemon p2 = new Pokemon("鴨頭", 10, 15, 20, 30);
+		System.out.println(p1 == p2); // false，因為兩個是不一樣的物件
+
+
         Pokemon x = new Pickachu("皮卡丘");
+
+        /*可以參考Java Api: https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/lang/Object.html*/
+        String toString = x.toString(); // 使用Object的toString方法
+        System.out.println(toString); // jave0515.Pickachu@4517d9a3，這是物件的識別號碼
+
         x.addCandy(100);
         x.levelUp();
         x.increaseAttackPower();
@@ -60,5 +72,7 @@ public class Gym {
         x.printInfo();
         y.printInfo();
         fight(x, y);
-    }
+
+
+	}
 }
