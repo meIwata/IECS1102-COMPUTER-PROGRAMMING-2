dbd35833ba7d57cf93ab62f29c4abcec16354131
waleilulu <waleilulu@gmail.com>
2025-05-28 23:08:25 +0800

    建立物件陣列

--- src/java0422/CardArray.java
+++ src/java0422/CardArray.java
@@ -2,6 +2,8 @@ package java0422;
 
 import java0415.IcCard;
 
+
+// 課本案例8-13
 public class CardArray {
     public static void main(String[] args) {
         IcCard[] manyCards = new IcCard[3];
@@ -9,7 +11,12 @@ public class CardArray {
             manyCards[i] = new IcCard();
             manyCards[i].number = String.valueOf(0x336789AB + i);
             manyCards[i].balance = 100 + i * 123;
+            manyCards[i].printInfo(); // 列印出資料
         }
+
+        System.out.println();
+
+        // 增強型for迴圈
         for(IcCard card: manyCards){
             card.printInfo();
         }
