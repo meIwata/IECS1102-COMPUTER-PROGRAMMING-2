913ff01fdc4e7657991ef8d8dbc591469ea5bbb9
meiwata <waleilulu@gmail.com>
2025-06-05 10:40:38 +0800

    lambda語法

--- src/java0605/Cat.java
+++ src/java0605/Cat.java
@@ -3,7 +3,7 @@ package java0605;
 interface Animal {
     void makeSound();
 
-    void sleep();
+//    void sleep();
 }
 
 class Toy {
@@ -19,16 +19,16 @@ public class Cat implements Animal {
         System.out.println("喵喵");
     }
 
-    @Override
-    public void sleep() {
-        System.out.println("喵喵正在睡覺");
-    }
+//    @Override
+//    public void sleep() {
+//        System.out.println("喵喵正在睡覺");
+//    }
 
     public static void main(String[] args) {
         // 實作Cat類別
         Cat kitty = new Cat();
         kitty.makeSound();
-        kitty.sleep();
+//        kitty.sleep();
 
         //----------------------------------
 
@@ -39,30 +39,39 @@ public class Cat implements Animal {
                 System.out.println("匿名類別1 的叫聲");
             }
 
-            @Override
-            public void sleep() {
-                System.out.println("匿名類別2 在睡覺");
-            }
+//            @Override
+//            public void sleep() {
+//                System.out.println("匿名類別2 在睡覺");
+//            }
         };
         animal.makeSound();
-        animal.sleep();
+//        animal.sleep();
 
         //----------------------------------
 
         Toy toy = new Toy();
         // 傳入 Cat 物件，呼叫 pressButton，會呼叫 Cat 的 makeSound()
         toy.pressButton(kitty); // 印出 "喵喵"
-        toy.pressButton(new Animal() {
+//        toy.pressButton(new Animal() {
+//
+//            @Override
+//            public void makeSound() {
+//                System.out.println("匿名類別2 的叫聲");
+//            }
+//
+//            @Override
+//            public void sleep() {
+//                System.out.println("匿名類別2 在睡覺");
+//            }
+//        });
 
-            @Override
-            public void makeSound() {
-                System.out.println("匿名類別2 的叫聲");
-            }
-
-            @Override
-            public void sleep() {
-                System.out.println("匿名類別2 在睡覺");
-            }
+        // 使用lambda語法
+        toy.pressButton(() -> {
+            System.out.println("匿名類別2 的叫聲");
         });
+
+
+        animal.makeSound();
+//        animal.sleep();
     }
 }
