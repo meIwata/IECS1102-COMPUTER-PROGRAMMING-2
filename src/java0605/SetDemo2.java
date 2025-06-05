ffd2a6b5c7d1041acd90f21659c17802c4ff03b5
meiwata <waleilulu@gmail.com>
2025-06-05 11:54:31 +0800

    Set - LinkedHashSet

---
+++ src/java0605/SetDemo2.java
@@ -0,0 +1,23 @@
+package java0605;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class SetDemo2 {
+    public static void main(String[] args) {
+        // Set 是一個介面（interface），它定義了一組不允許重複元素的集合操作方法： HashSet、LinkedHashSet、TreeSet
+        Set<String> student = new LinkedHashSet<>();
+
+        student.add("Tom");
+        student.add("Jack");
+        student.add("Mary");
+        student.add("Linda");
+        student.add("Alice");
+        student.add("Bob");
+
+        for (String s : student) {
+            System.out.println(s);
+        }
+        // 放進去的順序跟拿出來的順序一樣
+    }
+}
