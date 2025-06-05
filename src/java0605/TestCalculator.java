ce835b7d2994916beae562bb7cf1b9d47931b98e
meiwata <waleilulu@gmail.com>
2025-06-05 10:56:47 +0800

    lambda語法

---
+++ src/java0605/TestCalculator.java
@@ -0,0 +1,42 @@
+package java0605;
+
+interface Calculator {
+    int add(int a, int b);
+}
+
+class MyCalculator implements Calculator {
+    @Override
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
+
+public class TestCalculator {
+    public static void main(String[] args) {
+
+        //匿名類別
+        Calculator calc1 = new Calculator() {
+
+            @Override
+            public int add(int a, int b) {
+                return a + b;
+            }
+        };
+        System.out.println(calc1.add(1, 2));
+
+
+        // lambda方法: 要實作的方法只有一個才能使用lambda
+//        Calculator calculator = (int x, int y) -> {
+//            return x + y;
+//        };
+
+        // lambda方法更簡潔寫法
+        Calculator calc2 = (int x, int y) -> x + y;
+        System.out.println(calc2.add(5, 6));
+
+
+        // 有名稱的類別
+        Calculator calc3 = new MyCalculator();
+        System.out.println(calc3.add(3, 4));
+    }
+}
