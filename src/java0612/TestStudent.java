49cfc20fd96d1e9c715587d53ee20dc416708944
meiwata <waleilulu@gmail.com>
2025-06-12 09:51:16 +0800

    將多個學生物件加入一個集合（List）中，然後利用迴圈從集合中搜尋指定學號的學生，最後印出該學生的姓名

---
+++ src/java0612/TestStudent.java
@@ -0,0 +1,63 @@
+package java0612;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Student {
+    private String sId;
+    private String name;
+    private int age;
+
+    public Student(String sId) {
+        this.sId = sId;
+    }
+
+    public Student(String sId, String name, int age) {
+        this.sId = sId;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getsId() {
+        return sId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+public class TestStudent {
+    public static void main(String[] args) {
+        List<Student> oopClass = new ArrayList<>();
+        Student tom = new Student("S001", "Tom", 18);
+        Student mary = new Student("S002", "Mary", 20);
+        Student john = new Student("S003", "John", 21);
+
+        oopClass.add(tom);
+        oopClass.add(mary);
+        oopClass.add(john);
+
+
+        // 請從oopClass找出學號為"S002"的學生，並印出他的姓名
+        // 提示：用迴圈找出符合條件的學生條件，再印出學生姓名
+        for (Student student : oopClass) {
+            if (student.getsId().equals("S002")) {
+                System.out.println("學生的姓名: " + student.getName());
+                break;
+            }
+        }
+    }
+}
