8ef2d193d7a12923a7d4255b6befbf2ecb7f2157
meiwata <waleilulu@gmail.com>
2025-06-12 10:35:14 +0800

    Map（鍵值對）來管理學生資料，並展示不同遍歷 Map 的方法

--- src/java0612/TestStudent2.java
+++ src/java0612/TestStudent2.java
@@ -44,6 +44,7 @@ public class TestStudent2 {
         Student2 tom = new Student2("S011", "Tom", 18);
         Student2 mary = new Student2("S008", "Mary", 20);
         Student2 john = new Student2("S003", "John", 21);
+        Student2 sandy = new Student2("S011", "Sandy", 21);
 
 //        Map<String, Student2> oopClass = new HashMap<>(); // 放進去跟拿出來的順序不一樣
 //        Map<String, Student2> oopClass = new LinkedHashMap<>(); // 放進去跟拿出來的順序一樣
@@ -51,13 +52,23 @@ public class TestStudent2 {
         oopClass.put("S011", tom);
         oopClass.put("S008", mary);
         oopClass.put("S003", john);
+        oopClass.put("S011", sandy); // 因為key相同，後面的資料會把前面的資料蓋掉
 
         System.out.println("現在裡面有: " + oopClass.size() + " 組資料");
 
+        // 拿到鍵再去找值
         Set<String> ids = oopClass.keySet();
         for (String studentId : ids) {
             Student2 student = oopClass.get(studentId);
             System.out.println(student.getsId() + ", " + student.getName() + ", " + student.getAge());
+        }
+
+        System.out.println("----------------");
+
+        // 依序把 Map 裡面所有「學號對學生」的資料一組一組拿出來，每一次就拿一對
+        Set<Map.Entry<String, Student2>> entries = oopClass.entrySet();
+        for (Map.Entry<String, Student2> entry : entries) {
+            System.out.println(entry.getKey() + ", " + entry.getValue().getName() + ", " + entry.getValue().getAge());
         }
 
 
