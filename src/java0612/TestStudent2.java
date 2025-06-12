1f2452933d42e667d1a484890285da658acfea6d
meiwata <waleilulu@gmail.com>
2025-06-12 10:18:32 +0800

    三種不同 Map 實作類別（HashMap、LinkedHashMap、TreeMap）在資料儲存與取出時的順序差異

--- src/java0612/TestStudent2.java
+++ src/java0612/TestStudent2.java
@@ -1,7 +1,6 @@
 package java0612;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 class Student2 {
     private String sId;
@@ -42,16 +41,28 @@ class Student2 {
 public class TestStudent2 {
     public static void main(String[] args) {
         // 使用鍵值對方式
-        Student2 tom = new Student2("S001", "Tom", 18);
-        Student2 mary = new Student2("S002", "Mary", 20);
+        Student2 tom = new Student2("S011", "Tom", 18);
+        Student2 mary = new Student2("S008", "Mary", 20);
         Student2 john = new Student2("S003", "John", 21);
 
-        Map<String, Student2> oopClass = new HashMap<>();
-        oopClass.put("S001", tom);
-        oopClass.put("S002", mary);
+//        Map<String, Student2> oopClass = new HashMap<>(); // 放進去跟拿出來的順序不一樣
+//        Map<String, Student2> oopClass = new LinkedHashMap<>(); // 放進去跟拿出來的順序一樣
+        Map<String, Student2> oopClass = new TreeMap<>(); // 放進去跟拿出來的順序會幫你排序好
+        oopClass.put("S011", tom);
+        oopClass.put("S008", mary);
         oopClass.put("S003", john);
 
-        Student2 student2 = oopClass.get("S002");
-        System.out.println("學生的姓名: " + student2.getName());
+        System.out.println("現在裡面有: " + oopClass.size() + " 組資料");
+
+        Set<String> ids = oopClass.keySet();
+        for (String studentId : ids) {
+            Student2 student = oopClass.get(studentId);
+            System.out.println(student.getsId() + ", " + student.getName() + ", " + student.getAge());
+        }
+
+
+//        Student2 student2 = oopClass.get("S002");
+//        System.out.println("學生的姓名: " + student2.getName());
+
     }
 }
