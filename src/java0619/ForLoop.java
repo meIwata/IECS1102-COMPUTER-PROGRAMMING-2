b3777252ffbc213a59342d0d77394902fc8e5f7c
meiwata <waleilulu@gmail.com>
2025-06-19 14:23:15 +0800

    6/19補課

--- src/java0619/ForLoop.java
+++ src/java0619/ForLoop.java
@@ -2,6 +2,8 @@ package java0619;
 
 public class ForLoop {
     public static void main(String[] args) {
-
+        for (int i = 1, j = 10; i <= 5 && j >= 5; i++, j--) {
+            System.out.println("i: " + i + ", j: " + j);
+        }
     }
 }
---
+++ src/java0619/WhileLoop.java
@@ -0,0 +1,11 @@
+package java0619;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int count = 1;
+        while (count <= 5) {
+            System.out.println("Count: " + count);
+            count++;
+        }
+    }
+}
